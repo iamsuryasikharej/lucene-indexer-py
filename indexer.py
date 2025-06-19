64ddf3992e82de0b10de19df324b84bc9a379812
iamsuryasikharej <iamssrofficial@gmail.com>
2025-06-19 05:23:25 +0000

    inverted index implemented

--- indexer.py
+++ indexer.py
@@ -5,12 +5,13 @@ import lucene
 import lucene
 lucene.initVM()
 from org.apache.lucene.analysis.core import WhitespaceAnalyzer
+
 from org.apache.lucene.analysis.tokenattributes import CharTermAttribute
 
 
 
 
-
+inverted_index={}
 
 porter=nltk.PorterStemmer()
 reader=None
@@ -24,16 +25,35 @@ with open('./wiki_movie_plots.csv',encoding='utf-8') as f:
 def make_inverted_index(path_to_file):
     csv_file=pd.read_csv(path_to_file)
     
-    for movie in csv_file['Plot']:
+    for index, row in csv_file.iterrows():
+        movie=row["Plot"]
+        id=row["Title"]
         
         words=nltk.word_tokenize(movie)
         lower_case_tokens=[w.lower() for w in words if w.isalnum()]
         # print(lower_case_tokens)
-        pos_tagged_tokens=nltk.pos_tag(words)
+        pos_tagged_tokens=nltk.pos_tag(lower_case_tokens)
+
+        # for item in pos_tagged_tokens:
+        #     print(item)
 
         stemmd_tokens=[porter.stem(w) for w in lower_case_tokens]
-        print(stemmd_tokens)
+        for token in stemmd_tokens:
+            if(inverted_index.get(token)==None):
+                inverted_index[token]=[{id},1]
+            else:
+                inverted_index[token][0].add(str(id))
+                inverted_index[token][1]=inverted_index[token][1]+1
+                
+    print("processed all docs")        
+    for index in inverted_index:
+        print(index)   
+
+
         
+      
+
+
 
 # make_inverted_index(path_to_file)
 
@@ -53,12 +73,12 @@ def make_inverted_index_using_lucene(path_to_file):
 
 
    
-
-
 def print_tokens(tokens,term_attr):
     tokens.reset()
     while tokens.incrementToken():
         print(term_attr.toString())
 
-make_inverted_index_using_lucene(path_to_file)
 
+
+
+make_inverted_index(path_to_file)
